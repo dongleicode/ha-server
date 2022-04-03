d74f3e16fe7ff49dde1ada8747b5c7bcbf0595ee
dongleicode <mhwgodl@163.com>
2022-04-03 17:41:00 +0800

    修改默认地址

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var logColor = {
     red: '\x1B[31m%s\x1B[0m',
 };
 module.exports = function () {
-    process.env.projectTarget = argv.target || 'http://130.1.15.79:9080/hrbmbank_07';//代理目标路径，默认手机银行79测试环境
+    process.env.projectTarget = argv.target || 'http://130.1.15.53:9080/hrbmbank_07';//代理目标路径，默认手机银行79测试环境
     process.env.projectPort = argv.port || 9000;//端口，默认9000
     process.env.projectRouter  = argv.router || '/hrbmbank';//默认根路由
     process.env.projectPath = path.resolve('./'); //可能是相对路径，要resolve，以便在gulpfile.js中使用
